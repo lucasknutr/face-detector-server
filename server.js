f2d055ca6e84a394d45e415a12e42aa05a15eb87
Lucas <lucasclaudinocc@gmail.com>
2023-08-11 10:38:20 -0300

    Just added some base files

---
+++ server.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import express from 'express';
+import database from '../../my-server/server-express.js'
+const app = express();
+
+
+console.log(database);
+
+// /signin --> push
+// /register --> push
