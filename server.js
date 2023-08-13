9b185c5916146bdc5df4507d81b6f01163dc7b38
Lucas <lucasclaudinocc@gmail.com>
2023-08-12 22:33:32 -0300

    COMMITING

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 import fs from 'fs';
 import express from 'express';
 import database from '../../my-server/server-express.js'
+
 const app = express();
+app.use(express.json());
 
+console.log(database[0])
 
-console.log(database);
+app.get('/', (req, res) => {
+    res.send('THIS IS WORKING');
+})
 
+app.post('/signin', (req, res) => {
+    if(req.body.email === database[0].email){
+        res.send('OH YEAH YOU SUCEEDED YOUR MFFFFFFFFFF I LOVE YOU B DA S')
+    } else (
+        res.send('error')
+    )
+})
+
+app.listen(3001, () => {
+    console.log('successsssssss');
+});
 // / --> get
 // /signin --> push
 // /register --> push
 
-// test
