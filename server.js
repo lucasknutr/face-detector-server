0f34373e6715a33cbfc8f0c25c87a4ce17458c6c
Lucas <lucasclaudinocc@gmail.com>
2023-08-12 23:57:52 -0300

    sou apenas um rapaz latino americano

--- server.js
+++ server.js
@@ -5,22 +5,29 @@ import database from '../../my-server/server-express.js'
 const app = express();
 app.use(express.json());
 
-console.log(database[0])
-
 app.get('/', (req, res) => {
     res.send('THIS IS WORKING');
 })
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database[0].email){
-        res.send('OH YEAH YOU SUCEEDED YOUR MFFFFFFFFFF I LOVE YOU B DA S')
-    } else (
-        res.send('error')
-    )
+    let response = false;
+    database.forEach(user => {
+
+        // todo ADD: && req.body.password === user.password
+
+        if(req.body.email === user.email){
+            res.send('success!');
+            response = true;
+        }
+    });
+    if(response === false){
+        res.send('error!');
+    }
+
 })
 
 app.listen(3001, () => {
-    console.log('successsssssss');
+    console.log('successsssssss!');
 });
 // / --> get
 // /signin --> push
